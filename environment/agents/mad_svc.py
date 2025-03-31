a351ab089a20c0e520425194539c2fd759b4bc29
Hengji-cs <hengjizhou01@gmail.com>
2025-03-31 10:31:24 +0800

    Update mad_svc.py

--- environment/agents/mad_svc.py
+++ environment/agents/mad_svc.py
@@ -17,6 +17,7 @@ class MadSVCAgent:
         self.lyrics = config["mad_svc"]["lyrics"]
         self.target = config["mad_svc"]["target"]
         self.reqs = config["mad_svc"]["reqs"]
+        self.bgm = config["mad_svc"]["bgm"]
 
         self.normalizer = LoudnessNormalizer()
         self.mixer = MadSVCMixer()
@@ -28,46 +29,45 @@ class MadSVCAgent:
         self.subtitle = MadSVCSubtitle()
 
     def orchestrator(self):
-        # pre_msg = Message(content={"audio_dir": os.path.dirname(self.target)})
-        # loudness_result = self.normalizer.process_message(pre_msg)
-        # print(self.midi)
-        # annotator_msg = Message(content={"midi": self.midi, "lyrics": self.lyrics})
-        # annotator_result = self.annotator.process_message(annotator_msg)
+        pre_msg = Message(content={"audio_dir": os.path.dirname(self.target)})
+        loudness_result = self.normalizer.process_message(pre_msg)
+        annotator_msg = Message(content={"midi": self.midi, "lyrics": self.lyrics})
+        annotator_result = self.annotator.process_message(annotator_msg)
 
-        # analyzer_msg = Message(content={"annotator_result": annotator_result.content, "reqs": self.reqs})
-        # analyzer_result = self.analyzer.process_message(analyzer_msg)
+        analyzer_msg = Message(content={"annotator_result": annotator_result.content, "reqs": self.reqs})
+        analyzer_result = self.analyzer.process_message(analyzer_msg)
 
-        # with open('dataset/mad_svc/script.txt', 'r', encoding='utf-8') as f:
-        #     script = f.read()
-        # analyzer_result = Message(content={"lyrics": script})
-        # spliter_msg = Message(content={"annotator_result": annotator_result, "analyzer_result": analyzer_result})
-        # spliter_result = self.spliter.process_message(spliter_msg)
+        with open('dataset/mad_svc/script.txt', 'r', encoding='utf-8') as f:
+            script = f.read()
+        analyzer_result = Message(content={"lyrics": script})
+        spliter_msg = Message(content={"annotator_result": annotator_result, "analyzer_result": analyzer_result})
+        spliter_result = self.spliter.process_message(spliter_msg)
 
         name = os.path.basename(self.midi)
         pure_name = os.path.splitext(name)[0]
-        # trans_msg = Message(content={"name": pure_name})
-        # trans_result = self.translator.process_message(trans_msg)
+        trans_msg = Message(content={"name": pure_name})
+        trans_result = self.translator.process_message(trans_msg)
 
-        # new_name = annotator_result.content.get('name') + '_cover'
-        # cover_msg = Message(content={"source": f"dataset/mad_svc/cover/有何不可_cover.wav", "target": self.target})
-        # cover_msg = Message(content={"source": f"dataset/mad_svc/cover/{new_name}.wav", "target": self.target})
-        # cover_result = self.cover.process_message(cover_msg)
-        # cover_result = Message(content={'output_dir': 'dataset/mad_svc/final'})
+        new_name = annotator_result.content.get('name') + '_cover'
+        cover_msg = Message(content={"source": f"dataset/mad_svc/cover/有何不可_cover.wav", "target": self.target})
+        cover_msg = Message(content={"source": f"dataset/mad_svc/cover/{new_name}.wav", "target": self.target})
+        cover_result = self.cover.process_message(cover_msg)
+        cover_result = Message(content={'output_dir': 'dataset/mad_svc/final'})
 
-        # if self.bgm is not None:
-        #     pre_msg = Message(content={"audio_dir": cover_result.content.get('output_dir')})
-        #     loudness_result = self.normalizer.process_message(pre_msg)
-        #
-        #     mixer_message = Message(content={"bgm": self.bgm, "output_dir": cover_result.content.get('output_dir')})
-        #     mixer_result = self.mixer.process_message(mixer_message)
-        #     print(mixer_result)
+        if self.bgm is not None:
+            pre_msg = Message(content={"audio_dir": cover_result.content.get('output_dir')})
+            loudness_result = self.normalizer.process_message(pre_msg)
+
+            mixer_message = Message(content={"bgm": self.bgm, "output_dir": cover_result.content.get('output_dir')})
+            mixer_result = self.mixer.process_message(mixer_message)
+            print(mixer_result)
 
 
-        subtitle_msg = Message(content={
-            "annotation_path": 'dataset/mad_svc/lyrics_annotation/有何不可_cover.json',
-            "video_path": 'dataset/mad_svc/final/有何不可.mp4',
-            "output_path": 'dataset/mad_svc/final/有何不可_subtitle.mp4'})
-        subtitle_result = self.subtitle.process_message(subtitle_msg)
+        # subtitle_msg = Message(content={
+        #     "annotation_path": 'dataset/mad_svc/lyrics_annotation/有何不可_cover.json',
+        #     "video_path": 'dataset/mad_svc/final/有何不可.mp4',
+        #     "output_path": 'dataset/mad_svc/final/有何不可_subtitle.mp4'})
+        # subtitle_result = self.subtitle.process_message(subtitle_msg)
 
         return {
             "status": "success"
