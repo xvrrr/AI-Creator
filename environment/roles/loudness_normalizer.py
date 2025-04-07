60dbf5bab3ad342efe692ded84c104838c440910
Hengji-cs <hengjizhou01@gmail.com>
2025-04-07 23:01:02 +0800

    Update loudness_normalizer.py

--- environment/roles/loudness_normalizer.py
+++ environment/roles/loudness_normalizer.py
@@ -53,7 +53,7 @@ class LoudnessNormalizer(BaseAgent):
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True,
-                encoding='utf-8',  # 如果遇到编码问题，可以改为'gbk'
+                encoding='gbk',  # 如果遇到编码问题，可以改为'gbk'
                 bufsize=1
             )
 
